4f39ecea4245aa303e70a262d3b0fbb05433583f
sehan29 <sehangamage708@gmail.com>
2023-07-04 09:10:17 +0530

    Fetch Parties Data

--- user_operation.py
+++ user_operation.py
@@ -5,9 +5,14 @@ import main_fun as main_function
 from user_dash import administration_function
 from user_dash import comman_header
 from user_dash import registered_citizen_template
+from user_dash import registered_parties_template
 from citizen_class import Citizen
 import Insert_citizen_data as database_insert
 from fetch_citizen_data import Retrive_data
+from politicle_ import Politicle_part
+from registration_parties import Database
+
+
 def selectting_user_operation(selection):
     
     if(selection == 1):
@@ -46,11 +51,12 @@ def administration_operation(selection):
     
     if(selection == 1):
         print(selection)
-        
+        get_party_details()
         
     
     elif(selection == 2):
         print(selection)
+        get_candidator_details()
         
     elif(selection == 3):
         print(selection)
@@ -59,7 +65,10 @@ def administration_operation(selection):
         
     elif(selection == 4):
         print(selection)
+        os.system('cls')
+        view_registered_parties()
         
+             
     elif(selection == 5):
         print(selection)
         
@@ -108,4 +117,27 @@ def view_registered_citizen():
     registered_citizen_template()
     Retrive_data.fetch_data()
     
+    
+
+
+def get_pary_details():
+    
+    comman_header()
+    party_name = input("Enter Party Name : ")
+    reg_id = input("Registered ID Of Party : ")
+    president_name = input("Enter President Name of Party : ")
+    politicle_object = Politicle_part(party_name,president_name,reg_id)
+    database_obj = Database
+    database_obj.insert_party_data(politicle_object)
+    
+   
+   
+def view_registered_parties():
+    comman_header()
+    registered_parties_template()
+    obj = Retrive_data
+    obj.fetch_parties_data()
+    
+    
+def get_candidator_details():
     
